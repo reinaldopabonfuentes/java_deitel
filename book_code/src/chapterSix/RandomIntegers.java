efd21254ff97878a18bf835b48337ad49d8818be
reinaldopabonfuentes <reinaldopabonfuentes@gmail.com>
2023-09-04 07:32:12 -0500

    Added some classes

---
+++ book_code/src/chapterSix/RandomIntegers.java
@@ -0,0 +1,16 @@
+package chapterSix;
+
+// Fig. 6.5: RandomIntegers.java
+// Shifted and scaled random integers
+
+import java.security.SecureRandom; // program uses class SecureRandom
+
+public class RandomIntegers {
+    public static void main(String[] args){
+        // randomNumbers object will produce secure random numbers
+        SecureRandom randomNumbers = new SecureRandom();
+
+        // loop 20 times
+        for (int)
+    }
+}
