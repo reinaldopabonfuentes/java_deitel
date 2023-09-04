dd13e317aa9b7236a08155b7130a0df3eefe54a3
reinaldopabonfuentes <reinaldopabonfuentes@gmail.com>
2023-09-04 10:03:02 -0500

    Add last classes of chapter 6

--- book_code/src/chapterSix/RandomIntegers.java
+++ book_code/src/chapterSix/RandomIntegers.java
@@ -11,6 +11,16 @@ public class RandomIntegers {
         SecureRandom randomNumbers = new SecureRandom();
 
         // loop 20 times
-        for (int)
+        for (int counter = 1; counter <= 20; counter++){
+            // pick random integer from 1 to 6
+            int face = 1 + randomNumbers.nextInt(6);
+
+            System.out.printf("%d  ", face); // display generated value
+
+            // if counter is divisible by 5, start a new line of output
+            if (counter % 5 == 0){
+                System.out.println();
+            }
+        }
     }
 }
