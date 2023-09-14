89414e28e66d78bf21fee2ee222fe778cc14a9ee
reinaldopabonfuentes <reinaldopabonfuentes@gmail.com>
2023-09-14 08:58:51 -0500

    Add method to demonstrate Static Imports

--- book_code/src/chapterEight/staticEmployee/EmployeeTest.java
+++ book_code/src/chapterEight/staticEmployee/EmployeeTest.java
@@ -1,6 +1,9 @@
 package chapterEight.staticEmployee;
 // Fig. 8.13: EmployeeTest.java
 // static member demonstration
+// Fig. 8.14: StaticImportTest.java
+// Static import of Math class methods
+import static java.lang.Math.*;
 
 public class EmployeeTest {
     public static void main(String[] args){
@@ -24,5 +27,20 @@ public class EmployeeTest {
                 e1.getFirstName(), e1.getLastName(),
                 e2.getFirstName(), e2.getLastName());
 
-    } // end class EmployeeTest
-}
+        System.out.println();
+
+        // Call Method to demonstrate static Imports
+        System.out.println("Method to demonstrate static Imports");
+        staticImport();
+
+    }
+
+    // demonstrate the static import of Math class methods
+
+    public static void staticImport(){
+        System.out.printf("sqrt(900.0) = %.1f%n", sqrt(900.0));
+        System.out.printf("ceil(-9.8) = %1f%n", ceil(-9.8));
+        System.out.printf("E = %f%n", E);
+        System.out.printf("PI = %f%n", PI);
+    }
+}// end class EmployeeTest
